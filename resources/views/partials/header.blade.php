70730575ff0fc215e408ac5e2ab0d63281fae45f
CraigChilds94 <childscraig17@gmail.com>
2015-06-19 16:51:31 +0100

    Find a location and mark it or mark where clicked

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -8,7 +8,7 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/vue/0.12.1/vue.min.js"></script>
-    <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+    <script src="http://maps.google.com/maps/api/js?sensor=true&libraries=places"></script>
     <script src="/js/all.js"></script>
 
     <title>@yield('title')</title>
--- resources/views/pins/create.blade.php
+++ resources/views/pins/create.blade.php
@@ -12,13 +12,13 @@
     </div>
 
     <script>
-        var map = new Vue({
+        var Map = new Vue({
 
             el: '#google-map',
 
             // Data for this view
             data: {
-                pin: '',
+                pin: false,
                 map: new GMaps({
                     div: '#google-map',
                     lat: -12.043333,
@@ -26,17 +26,63 @@
                 })
             },
 
-            // Methods for this view
             methods: {
 
-                createPin: function(e) {
-                    console.log('hello mummy');
+                onClick: function(event) {
+
+                    // Grab click locations
+                    var lat = event.latLng.lat(),
+                        lng = event.latLng.lng();
+
+                    // Build places request
+                    var location = new google.maps.LatLng(lat, lng);
+                    var request = {
+                        location: location,
+                        radius: 10,
+                        types: ['store']
+                    };
+
+                    // Default marker to add
+                    var marker = {
+                        lat: lat,
+                        lng: lng,
+                        title: 'Custom Marker'
+                    };
+
+                    var self = this;
+
+                    // Make a request to the places API to see if we can
+                    // find a nearby place.
+                    var service = new google.maps.places.PlacesService(this.map.map);
+                    service.nearbySearch(request, function(results, status) {
+
+                        if (status == google.maps.places.PlacesServiceStatus.OK) {
+
+                            if(results.length > 1) {
+                                var place = results[0];
+
+                                marker = {
+                                    lat: place.geometry.location.lat(),
+                                    lng: place.geometry.location.lng(),
+                                    title: 'Place'
+                                };
+                            }
+
+                        }
+
+                        self.map.removeMarkers();
+                        self.map.addMarker(marker);
+                        self.pin = marker;
+
+                    });
+
                 }
 
             }
 
-
         });
+
+        Map.map.on('click', Map.onClick);
     </script>
 
 @stop
